00effd4e22515dd313c79cae2208a4792375d46f
grimerssy <stanislav.stoianov@nure.ua>
2022-07-19 12:11:40 +0300

    fix logger signature

--- internal/mw/log/logger.go
+++ internal/mw/log/logger.go
@@ -4,12 +4,11 @@ import (
 	"time"
 
 	"github.com/grimerssy/go-example/pkg/log"
-	"google.golang.org/grpc/codes"
 )
 
 type logger interface {
-	Log(msg string, fields ...log.Field)
-	WithGrpcCode(code codes.Code) log.Field
+	Log(lvl log.Level, msg string, fields ...log.Field)
+	WithString(key, val string) log.Field
 	WithStrings(key string, ss []string) log.Field
 	WithDuration(key string, val time.Duration) log.Field
 }
--- internal/mw/log/unary.go
+++ internal/mw/log/unary.go
@@ -3,6 +3,7 @@ package log
 import (
 	"context"
 
+	"github.com/grimerssy/go-example/pkg/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
@@ -12,15 +13,62 @@ func UnaryServerInterceptor(logger logger) grpc.UnaryServerInterceptor {
 		handler grpc.UnaryHandler) (interface{}, error) {
 		stopTimer := startTimer()
 		res, err := handler(ctx, req)
-		logger.Log(err.Error(),
-			logger.WithGrpcCode(getGrpcCode(err)),
+		code := getGrpcCode(err)
+		logger.Log(
+			getLogLevel(code),
+			err.Error(),
+			logger.WithString("gRPC-code", code.String()),
 			logger.WithStrings("callers", getCallers(err)),
 			logger.WithDuration("time-taken", stopTimer()))
 		return res, err
 	}
 }
 
+func getLogLevel(code codes.Code) log.Level {
+	switch code {
+	case codes.OK:
+		return log.Info
+	case codes.Canceled:
+		return log.Info
+	case codes.Unknown:
+		return log.Error
+	case codes.InvalidArgument:
+		return log.Info
+	case codes.DeadlineExceeded:
+		return log.Warn
+	case codes.NotFound:
+		return log.Info
+	case codes.AlreadyExists:
+		return log.Info
+	case codes.PermissionDenied:
+		return log.Warn
+	case codes.ResourceExhausted:
+		return log.Warn
+	case codes.FailedPrecondition:
+		return log.Warn
+	case codes.Aborted:
+		return log.Warn
+	case codes.OutOfRange:
+		return log.Warn
+	case codes.Unimplemented:
+		return log.Error
+	case codes.Internal:
+		return log.Error
+	case codes.Unavailable:
+		return log.Warn
+	case codes.DataLoss:
+		return log.Error
+	case codes.Unauthenticated:
+		return log.Info
+	default:
+		panic("did not match gRpc-code")
+	}
+}
+
 func getGrpcCode(err error) codes.Code {
+	if err == nil {
+		return codes.OK
+	}
 	c, ok := err.(interface{ Code() codes.Code })
 	if !ok {
 		return codes.Unknown
